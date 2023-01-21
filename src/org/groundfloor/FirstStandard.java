4be2bde64ab2bdb4ce558559ffa0396aa25524e9
karthirajan <karthirajan0706@gmail.com>
2023-01-21 10:31:47 +0530

    new project

---
+++ src/org/groundfloor/FirstStandard.java
@@ -0,0 +1,30 @@
+package org.groundfloor;
+
+//pascal or init
+public class FirstStandard {
+	
+	//() - parenthesis
+	//camal 
+	public void kevinAndrew(){
+		
+		System.out.println("Wecome kevin");
+		
+	}
+	
+	
+	public void selvaMani(){
+		
+		System.out.println("Welcome selva");
+		
+	}
+	
+	//[] array
+	public static void main(String[] args){
+		
+		//classname obj = new classname();
+		FirstStandard f = new FirstStandard();
+		f.selvaMani();
+		f.kevinAndrew();
+	}
+
+}
